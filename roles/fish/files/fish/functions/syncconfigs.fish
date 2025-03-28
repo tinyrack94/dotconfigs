4319aa817d4662a2b59626c1b9678a81fce50684
winetree94 <winetree94@gmail.com>
2025-03-28 18:16:04 +0900

    20250328

--- roles/fish/files/fish/functions/syncconfigs.fish
+++ roles/fish/files/fish/functions/syncconfigs.fish
@@ -1,10 +1,11 @@
 function syncconfigs
-	set -l target $argv[1]
+    set -l target $argv[1]
 
-	switch $target
-		case "fish"
-			rsync -av --delete --exclude='bitwarden.fish' ~/.config/fish/ ~/.dotconfigs/roles/fish/files/fish
-	end
+    switch $target
+        case fish
+            rsync -vrl --delete --exclude='bitwarden.fish' ~/.config/fish/ ~/.dotconfigs/roles/fish/files/fish
+        case nvim
+            rsync -vrl --delete ~/.config/nvim/ ~/.dotconfigs/roles/nvim/files/nvim
+    end
 
 end
-
