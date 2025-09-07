59c342eb47574fd6b54b204d00d4a0e1846339a0
tinyrack94 <winetree94@gmail.com>
2025-09-08 01:29:06 +0900

    update

--- roles/zsh/files/zsh/configs.zsh
+++ roles/zsh/files/zsh/configs.zsh
@@ -2,4 +2,6 @@ export EDITOR=nvim
 export KUBECONFIG=~/.kube/k3s.yaml
 ZSH_THEME="miloshadzic"
 
+export PUBLIC_CONTROL_PLANE_IP=10.23.11.1
+
 git_clone_if_not_exists "https://github.com/nvm-sh/nvm.git" "--depth=1" "$HOME/.nvm"
