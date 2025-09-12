dce97f9a9416fce2d330d129181291b906137265
tinyrack94 <winetree94@gmail.com>
2025-09-13 01:02:12 +0900

    update

--- roles/zsh/files/zsh/configs.zsh
+++ roles/zsh/files/zsh/configs.zsh
@@ -6,4 +6,6 @@ export PUBLIC_CONTROL_PLANE_IP=10.23.11.1
 
 export TALOSCONFIG=/home/winetree94/workspaces/tinyrack/public/talos/talosconfig
 
+maybe_eval direnv hook zsh
+
 git_clone_if_not_exists "https://github.com/nvm-sh/nvm.git" "--depth=1" "$HOME/.nvm"
