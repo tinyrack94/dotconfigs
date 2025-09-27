58a58a31d7e809d3f1bf98e3442e1242b113e5aa
tinyrack94 <winetree94@gmail.com>
2025-09-27 15:58:50 +0900

    update

--- roles/nvim/files/nvim/plugin/lsp.lua
+++ roles/nvim/files/nvim/plugin/lsp.lua
@@ -1,13 +1,13 @@
 local add = MiniDeps.add
 
 add({
-  source = "folke/lazydev.nvim"
+  source = "folke/lazydev.nvim",
 })
 
 require("lazydev").setup({
   library = {
     { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-    { path = "MiniDeps",           words = { "MiniDeps" } },
+    { path = "MiniDeps", words = { "MiniDeps" } },
   },
 })
 
@@ -15,8 +15,8 @@ add({
   source = "mason-org/mason.nvim",
   depends = {
     "neovim/nvim-lspconfig",
-    "mason-org/mason-lspconfig.nvim"
-  }
+    "mason-org/mason-lspconfig.nvim",
+  },
 })
 
 require("mason").setup({})
@@ -48,10 +48,10 @@ require("mason-lspconfig").setup({
 })
 
 add({
-  source = "WhoIsSethDaniel/mason-tool-installer"
+  source = "WhoIsSethDaniel/mason-tool-installer",
 })
 
-require('mason-tool-installer').setup({
+require("mason-tool-installer").setup({
   ensure_installed = {
     "eslint_d",
     "prettierd",
@@ -59,6 +59,39 @@ require('mason-tool-installer').setup({
     "stylelint",
     "ansible-lint",
     "jsonlint",
-    "markdownlint"
-  }
+    "markdownlint",
+  },
+})
+
+-- kubernetes
+
+add({
+  source = "diogo464/kubernetes.nvim",
+})
+
+require("kubernetes").setup({
+  -- this can help with autocomplete. it sets the `additionalProperties` field on type definitions to false if it is not already present.
+  schema_strict = true,
+  -- true:  generate the schema every time the plugin starts
+  -- false: only generate the schema if the files don't already exists. run `:KubernetesGenerateSchema` manually to generate the schema if needed.
+  schema_generate_always = true,
+  -- Patch yaml-language-server's validation.js file.
+  patch = true,
+  -- root path of the yamlls language server. by default it is assumed you are using mason but if not this option allows changing that path.
+  yamlls_root = function()
+    return vim.fs.joinpath(vim.fn.stdpath("data"), "/mason/packages/yaml-language-server/")
+  end,
+})
+
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = {
+      schemas = {
+        -- use this if you want to match all '*.yaml' files
+        [require("kubernetes").yamlls_schema()] = "*.yaml",
+        -- or this to only match '*.<resource>.yaml' files. ex: 'app.deployment.yaml', 'app.argocd.yaml', ...
+        -- [require("kubernetes").yamlls_schema()] = require("kubernetes").yamlls_filetypes(),
+      },
+    },
+  },
 })
